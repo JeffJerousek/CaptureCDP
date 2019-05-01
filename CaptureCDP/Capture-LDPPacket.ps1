bfaa9624f90b8c5ae463d6f0c8708444e9e05dee
Jeff Jerousek <37905627+JeffJerousek@users.noreply.github.com>
2019-05-01 10:09:45 -0500

    LDP in progress

---
+++ CaptureCDP/Capture-LDPPacket.ps1
@@ -0,0 +1,44 @@
+﻿ #Capture-LDPPacket
+ [Int16]$Duration = 62
+ try {
+                $CimSession = New-CimSession -ComputerName $env:COMPUTERNAME -ErrorAction Stop
+            } catch {
+                Write-Warning "Unable to create CimSession. Please make sure WinRM and PSRemoting is enabled on $Computer."
+                continue
+            }
+
+            $ETLFile = Invoke-Command -ComputerName $env:COMPUTERNAME -ScriptBlock {
+                $TempFile = New-TemporaryFile
+                Rename-Item -Path $TempFile.FullName -NewName $TempFile.FullName.Replace('.tmp', '.etl') -PassThru
+            }
+
+            $Adapter = Get-NetAdapter -Physical -CimSession $CimSession | 
+                Where-Object {$_.Status -eq 'Up' -and $_.InterfaceType -eq 6} | 
+                Select-Object -First 1 -ExpandProperty Name
+
+            if ($Adapter) {
+                $Session = New-NetEventSession -Name LDP -LocalFilePath 'C:\temp\temp2.etl' -CaptureMode SaveToFile -CimSession $CimSession
+
+                Add-NetEventPacketCaptureProvider -SessionName LDP -EtherType 0x88CC -TruncationLength 1024 -CaptureType BothPhysicalAndSwitch -CimSession $CimSession | Out-Null
+                Add-NetEventNetworkAdapter -Name $Adapter -PromiscuousMode $True -CimSession $CimSession | Out-Null
+
+                Start-NetEventSession -Name LDP -CimSession $CimSession
+
+                $Seconds = $Duration
+                $End = (Get-Date).AddSeconds($Seconds)
+                while ($End -gt (Get-Date)) {
+                    $SecondsLeft = $End.Subtract((Get-Date)).TotalSeconds
+                    $Percent = ($Seconds - $SecondsLeft) / $Seconds * 100
+                    Write-Progress -Activity "CDP Packet Capture" -Status "Capturing on $env:COMPUTERNAME..." -SecondsRemaining $SecondsLeft -PercentComplete $Percent
+                    [System.Threading.Thread]::Sleep(500)
+                }
+
+                Stop-NetEventSession -Name LDP -CimSession $CimSession
+
+                $Log = Invoke-Command -ComputerName $env:COMPUTERNAME -ScriptBlock {
+                    Get-WinEvent -Path $args[0] -Oldest | 
+                    Where-Object {$_.Id -eq 1001 -and [BitConverter]::ToUInt16($_.Properties[3].Value[13..10], 0) -eq [UInt16]0x88cc} |
+                        Select-Object -Last 1 -ExpandProperty Properties
+                } -ArgumentList $Session.LocalFilePath
+
+                }
---
+++ CaptureCDP/Parse-LDPPacket.ps1
@@ -0,0 +1,102 @@
+﻿function Parse-LDPPacket {
+
+<#
+
+.SYNOPSIS
+
+    Parse CDP packet returned from Capture-CDPPacket.
+
+.DESCRIPTION
+
+    Parse CDP packet to get port, switch, model, ipaddress and vlan.
+
+.PARAMETER Packet
+
+    Array of one or more byte arrays from Capture-CDPPacket.
+
+.EXAMPLE
+
+    PS> $Packet = Capture-CDPPacket
+    PS> Parse-CDPPacket -Packet $Packet
+
+    Port      : FastEthernet0/1 
+    Switch    : SWITCH1.domain.example 
+    Model     : cisco WS-C2960-48TT-L 
+    IPAddress : 192.0.2.10
+    VLAN      : 10
+
+.EXAMPLE
+
+    PS> Capture-CDPPacket -Computer COMPUTER1 | Parse-CDPPacket
+
+    Port      : FastEthernet0/1 
+    Switch    : SWITCH1.domain.example 
+    Model     : cisco WS-C2960-48TT-L 
+    IPAddress : 192.0.2.10
+    VLAN      : 10
+
+.EXAMPLE
+
+    PS> 'COMPUTER1', 'COMPUTER2' | Capture-CDPPacket | Parse-CDPPacket
+
+    Port      : FastEthernet0/1 
+    Switch    : SWITCH1.domain.example 
+    Model     : cisco WS-C2960-48TT-L 
+    IPAddress : 192.0.2.10
+    VLAN      : 10
+
+    Port      : FastEthernet0/2 
+    Switch    : SWITCH1.domain.example 
+    Model     : cisco WS-C2960-48TT-L 
+    IPAddress : 192.0.2.10
+    VLAN      : 20
+
+#>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0, 
+            Mandatory=$true, 
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true)]
+        [object[]]$Packet
+    )
+
+    process {
+
+        $Offset = 14
+        $Hash = @{}
+
+        while ($Offset -lt ($Packet.Length - 4)) {
+
+            $LLDP = [BitConverter]::ToUInt16($Packet[13..12], 0)
+
+            $Destination = '{0:X2}' -f $Packet[0] + ':' + '{0:X2}' -f $Packet[1] + ':' +'{0:X2}' -f $Packet[2] + ':' +'{0:X2}' -f $Packet[3] + ':' +'{0:X2}' -f $Packet[4] + ':' +'{0:X2}' -f $Packet[5]
+            $Source = '{0:X2}' -f $Packet[6] + ':' + '{0:X2}' -f $Packet[7]+ ':' +'{0:X2}' -f $Packet[8]+ ':' +'{0:X2}' -f $Packet[9]+ ':' +'{0:X2}' -f $Packet[10]+ ':' +'{0:X2}' -f $Packet[11]
+            #https://www.troliver.com/?p=348
+            $Type   = [BitConverter]::ToUInt16($Packet[($Offset + 1)..$Offset], 0)
+            $Length = [BitConverter]::ToUInt16($Packet[($Offset + 3)..($Offset + 2)], 0)
+
+            switch ($Type)
+            {
+                1  { $Hash.Add('Switch',    [System.Text.Encoding]::ASCII.GetString($Packet[($Offset + 4)..($Offset + $Length)])) }
+                2  { $Hash.Add('IPAddress', ([System.Net.IPAddress][byte[]]$Packet[($Offset + 13)..($Offset + 16)]).IPAddressToString) }
+                3  { $Hash.Add('Port',      [System.Text.Encoding]::ASCII.GetString($Packet[($Offset + 4)..($Offset + $Length)])) }
+                6  { $Hash.Add('Model',     [System.Text.Encoding]::ASCII.GetString($Packet[($Offset + 4)..($Offset + $Length)])) }
+                10 { $Hash.Add('VLAN',      [BitConverter]::ToUInt16($Packet[($Offset + 5)..($Offset + 4)], 0)) }
+            }
+
+	        if ($Length -eq 0 ) {
+		        $Offset = $Packet.Length
+	        }
+
+	        $Offset = $Offset + $Length
+
+        }
+
+        return [PSCustomObject]$Hash
+
+    }
+
+}
+#endregion
